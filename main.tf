2ffb9ca4c7e5b94ae1a0ad8176cc700fc2b8d826
Raul Naupari <raulnq@gmail.com>
2023-10-10 12:33:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,21 +8,18 @@ terraform {
 }
 
 provider "aws"{
-  region= "us-east-1"
-  access_key = "test"
-  secret_key = "test"
-  
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   
   endpoints {
-  s3             = "http://localhost:4566"
-	}
-
+    s3             = "http://localhost:4566"
+  }
 }
-
 
 resource "aws_s3_bucket" "test-bucket" {
   bucket = "my-bucket"
